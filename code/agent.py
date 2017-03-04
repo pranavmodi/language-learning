f172ac325d941ea4db6f2e91472d00f37743ca3c
Pranav Modi <pranav_modi@mckinsey.com>
2017-03-04 11:10:39 +0530

    update Game

--- code/agent.py
+++ code/agent.py
@@ -6,8 +6,6 @@ sys.path.append('tensorflow-vgg/')
 import utils
 import vgg16
 
-
-
 ## This class contains the Sender Agent which recieves the activations of the target image and distractor image, maps them into vocabulary words and sends it the reciever
 class SenderAgent:
 
@@ -15,13 +13,13 @@ class SenderAgent:
 
         self.vgg = vgg16.Vgg16()
 
+    def show_images(self, target, distractor):
+        images = tf.placeholder("float", [2, 224, 224, 3])
 
-    def show_images(target, disctractor):
-        image = tf.placeholder("float", [1, 224, 224, 3])
+        batch1 = target.reshape((1, 224, 224, 3))
+        batch2 = distractor.reshape((1, 224, 224, 3))
 
-        batch1 = img1.reshape((1, 224, 224, 3))
-        batch2 = img2.reshape((1, 224, 224, 3))
-
+        print(batch1.shape)
         batch = np.concatenate((batch1, batch2), 0)
 
         with tf.Session(
@@ -32,8 +30,8 @@ class SenderAgent:
             vgg = vgg16.Vgg16()
             with tf.name_scope("content_vgg"):
                 vgg.build(images)
+                prob = sess.run(vgg.prob, feed_dict=feed_dict)
 
-    prob = sess.run(vgg.prob, feed_dict=feed_dict)        
         comm_word = ''
         return comm_word
 
@@ -43,6 +41,7 @@ class RecieverAgent:
     def __init__(self, activation_shape=[1000, 1]):
         self.something = []
 
+        self.vgg = vgg16.Vgg16()
 
     def recieve(comm_word, image1, image2):
         return 0

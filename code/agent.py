854bc6d3d0027eae2c1d478fe1a736a8f8a1663b
Pranav Modi <pranav_modi@mckinsey.com>
2017-03-29 15:32:16 +0530

    resolve placeholder issue

--- code/agent.py
+++ code/agent.py
@@ -12,8 +12,10 @@ class SenderAgent:
     def __init__(self, vocab, embedding_dim = 2):
         self.vocab = vocab
         self.embedding_dim = embedding_dim
-
+        self.im_activations = tf.placeholder("float", [2, 1000])
+        self.vocab_scores = None
         self._build_image_to_vocab_mapper()
+
 
     def _build_image_to_vocab_mapper(self):
         #with tf.variable_scope('image_to_word'):
@@ -22,17 +24,17 @@ class SenderAgent:
         # 2. sigmoid non-linearity on embedding space
         # 3. fc-weights to concatenated vector for scores over vocacb
         with tf.name_scope('sender'):
-            self.im_activations = tf.placeholder("float", [2, 1000])
+
             weights1 = tf.Variable(tf.random_normal([1000, self.embedding_dim], stddev=0.1))
             embed_im = tf.nn.sigmoid(tf.matmul(self.im_activations, weights1))
             concat_im = tf.reshape(embed_im, [1, 2*self.embedding_dim])
-
             weights2 = tf.random_normal([2*self.embedding_dim, len(self.vocab)], stddev=0.1)
+            
             self.vocab_scores = tf.matmul(concat_im, weights2)
 
 
     def show_images(self, sess, target_acts, distractor_acts):
-        batch = np.concatenate([[target_acts, distractor_acts]], axis=0)
+        batch = np.concatenate([target_acts, distractor_acts], axis=0)
         v_scores = sess.run(self.vocab_scores, feed_dict={self.im_activations : batch})
         print(v_scores)
         comm_word = [0,1]

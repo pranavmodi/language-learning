a11cef0e2c30d50c5d52fb571d965e1c97536a69
Pranav Modi <pranav_modi@mckinsey.com>
2017-04-10 22:33:56 +0530

    combine sender, reciever graphs WIP

--- code/agent.py
+++ code/agent.py
@@ -9,135 +9,82 @@ import vgg16
 ## This class contains the Sender Agent which recieves the activations of the target image and distractor image, maps them into vocabulary words and sends it the reciever
 class Agents:
 
-    def __init__(self, vocab, embedding_dim = 2):
+    def __init__(self, vocab, image_embedding_dim = 2, embedding_dim = 2):
+
         self.vocab = vocab
-        self.embedding_dim = embedding_dim
-        self.im_activations = tf.placeholder("float", [2, 1000])
+
         self.target_acts = tf.placeholder("float", [None, 1000])
         self.distractor_acts = tf.placeholder("float", [None, 1000])
-        self.vocab_scores = None
-        self.game_scores = tf.placeholder(tf.float32, [None, 1])
-        #self._build_image_to_vocab_mapper()
-        self._trial_learning_graph()
+        self.image_acts = tf.placeholder("float", [None, 2000])
+
+        self.target = tf.placeholder(tf.int64, [None, 1])
+        #self.target = tf.placeholder("float", [None, 1])
+        self.image_embedding_dim = image_embedding_dim
+
+        self.game_scores = tf.placeholder("float", [None, 1])
+
+        self.vocab_scores = tf.placeholder("float", [None, len(self.vocab)])
+        self.embedding_dim = embedding_dim
+        #self.word = tf.placeholder(tf.int32, shape=())
+        self.im_activations = tf.placeholder("float", [2, 1000])
+        self.image_scores = tf.placeholder("float", [None, 2])
+
+        print('now building the learning graph')
+        self._build_learning_graph()
+        print('finished building the learning graph')
 
 
-    def _build_image_to_vocab_mapper(self):
-        #with tf.variable_scope('image_to_word'):
-        # Build a tf graph that -
-        # 1. image activations to embedding space
-        # 2. sigmoid non-linearity on embedding space
-        # 3. fc-weights to concatenated vector for scores over vocacb
-        with tf.name_scope('sender'):
-
-            target_weights = tf.Variable(tf.random_normal([1000, self.embedding_dim], stddev=0.1))
-            distractor_weights = tf.Variable(tf.random_normal([1000, self.embedding_dim], stddev=0.1))
-
-            #embed_im = tf.nn.sigmoid(tf.matmul(self.im_activations, weights1))
-            #concat_im = tf.reshape(embed_im, [1, 2*self.embedding_dim])
-
-            t_embed = tf.nn.sigmoid(tf.matmul(self.target_acts, target_weights))
-            d_embed = tf.nn.sigmoid(tf.matmul(self.distractor_acts, distractor_weights))
-
-            concat_im = tf.concat_v2([t_embed, d_embed], axis=1)
-
-            weights2 = tf.Variable(tf.random_normal([2*(self.embedding_dim), self.embedding_dim], stddev=0.1))
-
-            concat_embed = tf.matmul(concat_im, weights2)
-
-            word_embed = tf.Variable(tf.random_normal([len(self.vocab), self.embedding_dim], stddev=0.1))
-
-            word_dot = tf.mul(concat_embed, word_embed)
-            self.vocab_scores = tf.reduce_sum(word_dot, 1, keep_dims=True)
-            print(self.vocab_scores)
-            self.loss = tf.reduce_sum(self.game_scores)
-            print(self.loss)
-
-            self.optimizer = tf.train.RMSPropOptimizer(0.00025, 0.99, 0.0, 1e-6)
-            self.train_op = self.optimizer.minimize(self.loss)
-
-
-    def _trial_learning_graph(self):
-
+    def _build_learning_graph(self):
 
         with tf.name_scope('sender'):
-            weights = tf.Variable(tf.random_normal([2000, self.embedding_dim], stddev=0.1))
-            image_acts = tf.concat_v2([self.target_acts, self.distractor_acts], axis=1)
-            self.vocab_scores = tf.sigmoid(tf.matmul(image_acts, weights))
 
-            self.loss = tf.reduce_sum(self.game_scores)
+            ## Sender graph
+            weights1 = tf.Variable(tf.random_normal([2000, self.image_embedding_dim], stddev=0.1))
+            weights2 = tf.Variable(tf.random_normal([self.embedding_dim, len(self.vocab)], stddev=0.1))
+
+            ordered_acts = tf.concat_v2([self.target_acts, self.distractor_acts], axis=1)
+            h1 = tf.sigmoid(tf.matmul(ordered_acts, weights1))
+
+            self.vocab_scores = tf.matmul(h1, weights2)
+
+            word = tf.argmax(self.vocab_scores, axis=0)
+
+            ## Reciever graph
+            vocab_embedding = tf.Variable(tf.random_normal([len(self.vocab), self.embedding_dim]))
+            print('here?')
+            word_embed = tf.gather(vocab_embedding, word)
+            print('here?')
+            weights1 = tf.Variable(tf.random_normal([1000, self.embedding_dim], stddev=0.1))
+
+            print('coming till here')
+
+            #image_acts = tf.concatenate([self.im1, self.im2], axis=0)
+            #self.im_activations = tf.Print(self.image_acts, data = [self.im_activations.get_shape()], message='im activations shape')
+            #weights1 = tf.Print(weights1, data = [weights1.get_shape()], message='weights1 shape')
+
+            self.image_acts = tf.reshape(self.image_acts, [-1, 1000])
+            embed_im = tf.matmul(self.image_acts, weights1)
+            word_dot = tf.mul(embed_im, word_embed)
+            self.image_scores = tf.reduce_sum(word_dot, 1, keep_dims=True)
+
+            print(self.image_scores)
+            self.reciever_selects = tf.argmax(self.image_scores, axis=0)
+            #self.reciever_selects = tf.cast(self.reciever_selects, "float")
+
+            self.game_scores = tf.reduce_mean(tf.square(self.reciever_selects - self.target))
+
+            #self.loss = tf.reduce_sum(self.game_scores)
 
             self.optimizer = tf.train.RMSPropOptimizer(0.00025, 0.99, 0.0, 1e-6)
-            self.train_op = self.optimizer.minimize(self.loss)
+            self.train_op = self.optimizer.minimize(self.game_scores)
 
 
-    def show_images(self, sess, target_acts, distractor_acts):
-        batch = np.concatenate([target_acts, distractor_acts], axis=1)
-        v_scores = sess.run(self.vocab_scores, feed_dict={self.im_activations : batch})[0]
+    def show_images(self, sess, image_acts, target):
+        target_acts = image_acts[target]
+        distractor_acts = image_acts[1 - target]
+        v_scores, _ = sess.run([self.vocab_scores, self.train_op], feed_dict={self.target_acts : target_acts, self.distractor_acts : distractor_acts, self.image_acts : image_acts, self.target : target})
         comm_word = np.argmax(v_scores)
-        return comm_word
+        return (target, comm_word)
 
     def train_sender(sess, samples, rewards):
         return 0
-
-
-    def show_images_rec(self, sess, comm_word, image1, image2):
-        batch = np.concatenate((image1, image2), axis=0)
-        image_scores = sess.run(self.image_scores, feed_dict={self.word : comm_word, self.im_activations : batch})
-        selected = np.argmax(image_scores)
-        return selected
-
-
-    def _build_image_selection_with_word(self):
-        # Build a tf graph that -
-        # 1. image activations to embedding space
-        # 2. Symbol to embedding space
-        # 3. Dot product of symbol and each image
-
-        with tf.name_scope('reciever'):
-            vocab_embedding = tf.Variable(tf.random_normal([len(self.vocab), self.embedding_dim]))
-            word_embed = vocab_embedding[self.word, :]
-            weights1 = tf.Variable(tf.random_normal([1000, self.embedding_dim], stddev=0.1))
-            self.im_activations = tf.Print(self.im_activations, data = [self.im_activations.get_shape()], message='im activations shape')
-            weights1 = tf.Print(weights1, data = [weights1.get_shape()], message='weights1 shape')
-            embed_im = tf.matmul(self.im_activations, weights1)
-            embed_im = tf.Print(embed_im, data = [embed_im])
-            word_dot = tf.mul(embed_im, word_embed)
-            self.image_scores = tf.reduce_sum(word_dot, 1, keep_dims=True)    
-
-
-
-class RecieverAgent:
-    def __init__(self, vocab, activation_shape=[1000, 1], embedding_dim = 2):
-
-        self.vocab = vocab
-        self.embedding_dim = embedding_dim
-        self.word = tf.placeholder(tf.int32, shape=())
-        self.vgg = vgg16.Vgg16()
-        self.im_activations = tf.placeholder("float", [2, 1000])
-        self.image_scores = None
-        self._build_image_selection_with_word()
-        #self.embed_im = tf.placeholder(tf.float32, [2,2])
-
-    def show_images(self, sess, comm_word, image1, image2):
-        batch = np.concatenate((image1, image2), axis=0)
-        image_scores = sess.run(self.image_scores, feed_dict={self.word : comm_word, self.im_activations : batch})
-        selected = np.argmax(image_scores)
-        return selected
-
-
-    def _build_image_selection_with_word(self):
-        # Build a tf graph that -
-        # 1. image activations to embedding space
-        # 2. Symbol to embedding space
-        # 3. Dot product of symbol and each image
-
-        with tf.name_scope('reciever'):
-            vocab_embedding = tf.Variable(tf.random_normal([len(self.vocab), self.embedding_dim]))
-            word_embed = vocab_embedding[self.word, :]
-            weights1 = tf.Variable(tf.random_normal([1000, self.embedding_dim], stddev=0.1))
-            self.im_activations = tf.Print(self.im_activations, data = [self.im_activations.get_shape()], message='im activations shape')
-            weights1 = tf.Print(weights1, data = [weights1.get_shape()], message='weights1 shape')
-            embed_im = tf.matmul(self.im_activations, weights1)
-            embed_im = tf.Print(embed_im, data = [embed_im])
-            word_dot = tf.mul(embed_im, word_embed)
-            self.image_scores = tf.reduce_sum(word_dot, 1, keep_dims=True)

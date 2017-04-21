4f8ae31b7714b5a19d341d4e2a32aa28d3362e86
Pranav Modi <pranav.modi@gmail.com>
2017-04-21 10:12:18 +0000

    remove trial code

--- code/agent.py
+++ code/agent.py
@@ -23,7 +23,7 @@ class Agents:
 
         self.game_scores = tf.placeholder("float", [None, 1])
 
-        self.word_probs = tf.placeholder("float", [None, len(self.vocab)])
+        self.word_probs = tf.placeholder("float", [len(self.vocab), 1])
         self.embedding_dim = embedding_dim
 
         self.word = tf.placeholder(tf.int32, [], "word")
@@ -38,38 +38,6 @@ class Agents:
         self._build_learning_graph()
         print('finished building the learning graph')
 
-    def _build_trial_graph(self):
-
-        with tf.name_scope('trial'):
-
-            ## Sender graph
-            weights1 = tf.Variable(tf.random_normal([1000, self.image_embedding_dim], stddev=0.1), name = "sender_w1", trainable="true")
-            weights2 = tf.Variable(tf.random_normal([self.embedding_dim, len(self.vocab)], stddev=0.1), name = "sender_w2", trainable="true")
-
-            h1 = tf.sigmoid(tf.matmul(self.target_acts, weights1), name = "h1")
-            vocab_scores = tf.matmul(h1, weights2, name = "vocab_scores")
-
-            self.softmax_probs = tf.nn.softmax(self.word_probs)
-
-            cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=softmax_probs, labels=self.target), name = "cross_entropy")
-            self.train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-
-
-    def call_trial(self, sess, image_acts, target):
-        target_acts = image_acts[target]
-        distractor_acts = image_acts[1 - target]
-
-        target = np.reshape(target, [-1, 1])
-        image_acts = np.reshape(image_acts, [-1, 2000])
-        word_probs = sess.run([self.word_probs], feed_dict={self.target_acts : target_acts, self.distractor_acts : distractor_acts, self.image_acts : image_acts, self.target : target})
-
-        if random.random() < self.epsilon:
-            return random_action
-        word = np.random.choice(word_probs.shape[0], 1, p=word_probs)[0]
-
-        print(v_scores, word)
-
 
     def _build_learning_graph(self):
 
@@ -82,9 +50,13 @@ class Agents:
             ordered_acts = tf.concat_v2([self.target_acts, self.distractor_acts], axis=1)
             h1 = tf.sigmoid(tf.matmul(ordered_acts, weights1))
 
-            self.word_probs = tf.nn.softmax(tf.matmul(h1, weights2))
+            self.word_probs = tf.transpose(tf.nn.softmax(tf.matmul(h1, weights2)))
             self.optimizer = tf.train.AdamOptimizer(0.5)
+
+            self.word = tf.Print(self.word, [self.word], message='This is the word')
             selected_word_prob = tf.gather(self.word_probs, self.word)
+
+            #tf.Print(self.word, [self.word], message='gathering the right word prob')
             self.sender_loss = -tf.log(selected_word_prob) * self.reward
             self.sender_train_op = self.optimizer.minimize(self.sender_loss)
 
@@ -119,28 +91,31 @@ class Agents:
             #self.train_op = self.optimizer.minimize(accuracy)
 
 
-    def show_images(self, sess, image_acts, target):
+    def show_images(self, sess, image_acts, target, target_class):
         target_acts = image_acts[target]
         distractor_acts = image_acts[1 - target]
 
         word_probs = sess.run(self.word_probs, feed_dict={self.target_acts : target_acts, self.distractor_acts : distractor_acts})[0]
-
         print(word_probs)
+        word = np.random.choice(np.arange(len(self.vocab)), p=word_probs)
+        word_text = self.vocab[word]
+        print('selected word', word_text)
+        print(type(word_probs))
+        reward = 0
+        if target_class == 'dog':
+            if word_text == 'Dogword':
+                reward = 1.0
+        elif target_class == 'cat':
+            if word_text == 'Catword':
+                reward = 1.0
 
-        self.word = np.random.choice(np.arange(len(self.vocab)), p=word_probs)
+        print('the word is ', word)
+        print('the word type', type(word))
+        print(word_probs.shape)
+        #word_probs = np.reshape(word_probs, [-1, len(self.vocab)])
+        #print(word_probs.shape)
+        sess.run([self.sender_train_op, self.sender_loss], feed_dict={self.target_acts : target_acts, self.distractor_acts : distractor_acts, self.reward : reward, self.word : word, self.word_probs : word_probs})
 
-        if target==self.word:
-            self.reward = 1.0
-        else:
-            self.reward = 0.0
-
-        ## Now train the sender to select cat pics
-        
-
-
-        #print(self.word)
-        #v_scores, _ = sess.run([self.word_probs, self.train_step], feed_dict={self.target_acts : target_acts, self.distractor_acts : distractor_acts, self.image_acts : image_acts, self.target : target})
-        #comm_word = np.argmax(v_scores)
         return (target, self.word)
 
     def train_sender(sess, samples, rewards):
